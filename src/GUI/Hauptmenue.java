e61b918292f7d9867afa86911ae9c18a0874f9ad
benkroencke <benkroencke@web.de>
2018-11-25 15:34:04 +0100

    MainFrame und Panel ersetzt

--- src/GUI/Hauptmenue.java
+++ src/GUI/Hauptmenue.java
@@ -1,140 +1,95 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.Font;
-import javax.swing.JButton;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Hauptmenue extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Hauptmenue frame = new Hauptmenue();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+public class Hauptmenue extends JPanel {
 
 	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
 	public Hauptmenue() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		
-		JLabel lblHauptmen = new JLabel("Hauptmen\u00FC");
-		lblHauptmen.setHorizontalAlignment(SwingConstants.CENTER);
-		lblHauptmen.setFont(new Font("Tahoma", Font.BOLD, 48));
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		add(panel);
 		
-		JButton btnNewButton = new JButton("Kundenverwaltung");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton button = new JButton("Kundenverwaltung");
+		button.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		
+		JButton button_1 = new JButton("Ger\u00E4teverwaltung");
+		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EventQueue.invokeLater(new Runnable() {
-					public void run() {
-						try {
-							KundenverwaltungGUI frame = new KundenverwaltungGUI();
-							
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
-			// Hier muss noch das alter Fenster geschlossen werden.
+				
+				MainFrame.change(MainFrame.getHauptmenue(), MainFrame.getGeraeteTypVerwaltung());
+				
 			}
 		});
+		button_1.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		
-		JButton btnNewButton_1 = new JButton("Ger\u00E4teverwaltung");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		JButton button_2 = new JButton("Buchungsverwaltung");
+		button_2.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		
-		JButton btnBuchungsverwaltung = new JButton("Buchungsverwaltung");
-		btnBuchungsverwaltung.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		JButton button_3 = new JButton("W-Verwaltung");
+		button_3.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		
-		JButton btnRechnungsverwaltung = new JButton("Rechnungsverwaltung");
-		btnRechnungsverwaltung.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		JButton button_4 = new JButton("Rechnungsverwaltung");
+		button_4.setFont(new Font("Tahoma", Font.PLAIN, 28));
 		
-		JButton btnWirtschaftlichkeitsverwaltung = new JButton("W-Verwaltung");
-		btnWirtschaftlichkeitsverwaltung.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
+		JLabel label = new JLabel("Hauptmen\u00FC");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("Tahoma", Font.BOLD, 48));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 974, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 335, Short.MAX_VALUE)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(button, GroupLayout.DEFAULT_SIZE, 468, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+							.addComponent(button_1, GroupLayout.DEFAULT_SIZE, 472, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(btnBuchungsverwaltung, GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(button_2, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnWirtschaftlichkeitsverwaltung, GroupLayout.PREFERRED_SIZE, 274, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 274, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnRechnungsverwaltung, GroupLayout.DEFAULT_SIZE, 197, Short.MAX_VALUE)
+							.addComponent(button_4, GroupLayout.DEFAULT_SIZE, 337, Short.MAX_VALUE)
 							.addGap(2)))
 					.addGap(8))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(lblHauptmen, GroupLayout.DEFAULT_SIZE, 689, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(label, GroupLayout.DEFAULT_SIZE, 954, Short.MAX_VALUE)
 					.addContainerGap())
 		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(lblHauptmen, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 551, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(label, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_1, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+						.addComponent(button, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnWirtschaftlichkeitsverwaltung, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
-						.addComponent(btnBuchungsverwaltung, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
-						.addComponent(btnRechnungsverwaltung, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(button_3, GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)
+						.addComponent(button_2, GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)
+						.addComponent(button_4, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE))
 					.addGap(8))
 		);
-		contentPane.setLayout(gl_contentPane);
-		
-		
-		
-		btnBuchungsverwaltung.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				BuchungsverwaltungGUI frame = new BuchungsverwaltungGUI();
-				dispose();
-			}
-		});
-		
-		
-		
-		setVisible(true);
+		panel.setLayout(gl_panel);
+
 	}
+
 }
